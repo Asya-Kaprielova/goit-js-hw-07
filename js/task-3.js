194885538f256bceb14377532367433a2412d11a
Asya <nastena.kaprielova209@gmail.com>
2026-06-21 19:44:30 +0300

    first try

--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,6 @@
-class StringBuilder {
-    #value
-    constructor(initialValue ) {
-        this.#value = initialValue;
-    }
-    getValue() {
-        return this.#value;
-    }
-    padEnd(str) {
-        this.#value += str;
-    }
-    padStart(str) {
-        this.#value = str + this.#value;
-    }
-    padBoth(str) {
-        this.padStart(str);
-        this.padEnd(str);
-    }
-}
-
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+const name = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+name.addEventListener('input', () => {
+    const trimmedValue = event.target.value.trim();
+    nameOutput.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
